e3e676c2e13e5d8b96530e020135c161540bf044
Nick Inman <nick@theoatmeal.com>
2018-11-19 23:53:11 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,10 @@ def my_collect(array)
   new_array[]
   i = 0
   while i < array.length
-  array.i << new_array.cap
+  array(i).capitalize << new_array
     yield array[i]
     i = i + 1
   end
   array
+  new_array
 end
